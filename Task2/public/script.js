3071ca08561084d0af45b50c381db6f36ab50f14
Abhinav Tomar <ababhi7906@gmail.com>
2024-01-10 16:57:26 +0530

    initial commit

---
+++ Task2/public/script.js
@@ -0,0 +1,9 @@
+function performLogin() {
+    // Add your login logic here
+    alert('Login functionality to be implemented.');
+}
+
+function submitForm() {
+    // Add your form submission logic here
+    alert('Form submission functionality to be implemented.');
+}
